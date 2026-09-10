387fbd7844404aa091c49e9af5b82640454ee76c
Forrest Presnell <fpresne@siue.edu>
2026-09-09 22:26:55 -0500

    chore: update welcome banner B

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ def subtract(a, b):
     return a - b
 
 def multiply(a, b):
-    return a * b
+print(">>> Super Calculator: Version B <<<")
 
 def divide(a, b):
     if b == 0: return "Error: Division by zero"
